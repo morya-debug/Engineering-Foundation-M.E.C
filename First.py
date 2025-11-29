4d5d15b04941b18c779748d87f4b0708739a3659
morya754 <mouryaravi754@gmail.com>
2025-11-29 18:11:11 +0530

    1st commit

---
+++ First.py
@@ -0,0 +1,11 @@
+# Problem: Say "Hello, World" with Python 
+# Platform: HackerRank (Basic)
+# Link: https://www.hackerrank.com/challenges/py-hello-world/problem?isFullScreen=true
+
+# My Logic:
+# 1. I declared a variable x and Stored the string in it .
+# 2. I used print() to display the string in variable..
+
+x = "Hello, World!"
+print(x)
+
